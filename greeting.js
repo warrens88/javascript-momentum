feac258d908b4eb0a82584ec9f1206accd5b739b
warrens88 <sungminem@naver.com>
2021-05-18 18:04:06 +0900

    asdfasdf

--- greeting.js
+++ greeting.js
@@ -1,46 +1,19 @@
 const form = document.querySelector(".js-form"),
-  input = document.querySelector("input"),
-  greeting = document.querySelector(".js-greetings");
+      input = form.querySelector("input");
 
-//local storage 정보를 컴퓨터에 저장하는 방법
-const USER_LS = "currentUser",
-  SHOWING_CN = "showing";
-
-function saveName(text){
-  localStorage.setItem(USER_LS, text);
-}
-
-function handleSubmit(event){
-    event.preventDefault();
-    const currentValue = input.value;
-    paintGreeting(currentValue);
-    saveName(currentValue);
-}
-
-function askForName(){
-    form.classList.add(SHOWING_CN);
-    form.addEventListener("submit", handleSubmit);
-}
-
-function paintGreeting(text) {
-  form.classList.remove(SHOWING_CN);
-  greeting.classList.add(SHOWING_CN);
-  greeting.innerText = `Hello ${text}`;
-}
-
-function loadName() {
+const USER_LS = "currenUser"
+      
+function loadName(){
   const currentUser = localStorage.getItem(USER_LS);
-  if (currentUser === null) {
-    // she is not
-    askForName();
+  if(currentUser === null){
+     
   } else {
-    // she is
-    paintGreeting(currentUser);
+
   }
 }
 
-function init() {
-    loadName();
+function init(){
+  loadName();
 }
 
 init();
